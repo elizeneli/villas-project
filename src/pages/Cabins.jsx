5cde8e610c6d8bc7fbc75a1df2bd84d400c85e1c
elizeneli <74110499+elizeneli@users.noreply.github.com>
2024-11-02 14:38:30 +0100

    added delete & add cabin features

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,12 +1,26 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import CabinTable from "../features/cabins/CabinTable";
+import { useState } from "react";
+import Button from "../ui/Button";
+import CabinForm from "../../src/features/cabins/CreateCabinForm";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+      <Row style={{ flexDirection: "column" }}>
+        <CabinTable />
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add new Cabin
+        </Button>
+        {showForm && <CabinForm />}
+      </Row>
+    </>
   );
 }
 
