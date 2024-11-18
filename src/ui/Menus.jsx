fdabd6614f1021d276c340b48b825d84e0492953
elizeneli <74110499+elizeneli@users.noreply.github.com>
2024-11-18 23:59:51 +0100

    WIP

--- src/ui/Menus.jsx
+++ src/ui/Menus.jsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
-import { useContext } from "react";
-import { createContext } from "react";
+import { useState, useContext, createContext } from "react";
 import { createPortal } from "react-dom";
 import { HiEllipsisVertical } from "react-icons/hi2";
 import styled from "styled-components";
@@ -69,17 +67,30 @@ const MenusContext = createContext();
 
 function Menus({ children }) {
   const [openId, setOpenId] = useState("");
+  const [position, setPosition] = useState(null);
+
   const close = () => setOpenId("");
-  const open = setOpenId();
+  const open = (id) => setOpenId(id);
 
   return (
-    <MenusContext.Provider value={{ openId }}>{children}</MenusContext.Provider>
+    <MenusContext.Provider
+      value={{ openId, close, open, position, setPosition }}
+    >
+      {children}
+    </MenusContext.Provider>
   );
 }
 
 function Toggle({ id }) {
-  const { openId, close, open } = useContext(MenusContext);
-  function handleClick() {
+  const { openId, close, open, setPosition } = useContext(MenusContext);
+
+  function handleClick(e) {
+    const rect = e.target.closest("button").getBoundingClientRect();
+    setPosition({
+      x: window.innerWidth - rect.width - rect.x,
+      y: rect.y + rect.height + 8,
+    });
+
     openId === "" || openId !== id ? open(id) : close();
   }
   return (
@@ -90,11 +101,14 @@ function Toggle({ id }) {
 }
 
 function List({ id, children }) {
-  const { openId } = useContext(MenusContext);
+  const { openId, position } = useContext(MenusContext);
 
   if (openId !== id) return null;
 
-  return createPortal(<StyledList>{children}</StyledList>, document.body);
+  return createPortal(
+    <StyledList position={position}>{children}</StyledList>,
+    document.body
+  );
 }
 
 function Button({ children }) {
@@ -107,7 +121,7 @@ function Button({ children }) {
 
 Menus.Menu = Menu;
 Menus.Toggle = Toggle;
-Menu.List = List;
-Menu.Button = Button;
+Menus.List = List;
+Menus.Button = Button;
 
 export default Menus;
