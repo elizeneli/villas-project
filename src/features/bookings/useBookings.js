42cc1c02965b8e9930db672e257a1afa90dc2d36
elizeneli <74110499+elizeneli@users.noreply.github.com>
2024-11-25 21:04:07 +0100

    added filter on bookings based on status

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,22 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter], //this is like the dependency array of use effect hook, because it tells the react query to refetch the data from the filter everytime bookings its called
+    queryFn: () => getBookings({ filter }),
   });
   return { isLoading, error, bookings };
 }
