6f690fae9284e335c4d5ec3567d5ef0a07304b71
elizeneli <74110499+elizeneli@users.noreply.github.com>
2024-12-09 22:38:48 +0100

    WIP: Protected routes

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,4 +1,12 @@
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+
 function ProtectedRoute({ children }) {
+  //load the auth user
+  const { user, isLoading } = useUser();
+  if (isLoading) return <Spinner />;
+  // while loading show a spinner
+  //if there is no authenticated user, redirect to /login
   return children;
 }
 
