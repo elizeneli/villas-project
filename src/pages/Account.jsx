ee221b262071f6eac74dd51cf6aa730ae067273e
elizeneli <74110499+elizeneli@users.noreply.github.com>
2024-12-18 00:42:40 +0100

    Finished user avatar and user profile edit

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,3 +1,4 @@
+import UpdateUserDataForm from "../features/authentication/UpdateUserDataForm";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
@@ -6,10 +7,8 @@ function Account() {
     <>
       <Heading as="h1">Update your account</Heading>
 
-      <Row>
-        <Heading as="h3">Update user data</Heading>
-        <p>Update user data form</p>
-      </Row>
+      <Heading as="h3">Update user data</Heading>
+      <UpdateUserDataForm />
 
       <Row>
         <Heading as="h3">Update password</Heading>
