a111d929b7173b1941148ce1ed95b7f61e7bf153
elizeneli <74110499+elizeneli@users.noreply.github.com>
2024-12-21 20:57:23 +0100

    enabled dark mode according to user's sys.sett.

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -16,6 +16,7 @@ import { useCheckout } from "../check-in-out/useCheckout";
 import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
 import { useDeleteBooking } from "./useDeleteBooking";
+import Empty from "../../ui/Empty";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -30,8 +31,9 @@ function BookingDetail() {
   const navigate = useNavigate();
   const { checkout, isCheckingOut } = useCheckout();
   const { deleteBooking, isDeleting } = useDeleteBooking();
-  if (isLoading) return <Spinner />;
 
+  if (isLoading) return <Spinner />;
+  if (!booking) return <Empty resourceName="booking" />;
   const { status, id: bookingId } = booking;
   console.log("test", booking);
   const statusToTagName = {
