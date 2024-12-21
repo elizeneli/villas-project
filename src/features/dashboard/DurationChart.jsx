3894934cdf0b910392d8235f9ddd0f28f46cd23f
elizeneli <74110499+elizeneli@users.noreply.github.com>
2024-12-21 16:26:53 +0100

    WIP-(Dashboard): Displaying stay duration with pie chart

--- src/features/dashboard/DurationChart.jsx
+++ src/features/dashboard/DurationChart.jsx
@@ -1,4 +1,14 @@
 import styled from "styled-components";
+import Heading from "../../ui/Heading";
+import {
+  Cell,
+  Legend,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+} from "recharts";
+import { useDarkMode } from "../../context/DarkModeContext";
 
 const ChartBox = styled.div`
   /* Box */
@@ -21,43 +31,43 @@ const ChartBox = styled.div`
 const startDataLight = [
   {
     duration: "1 night",
-    value: 0,
+    value: 1,
     color: "#ef4444",
   },
   {
     duration: "2 nights",
-    value: 0,
+    value: 2,
     color: "#f97316",
   },
   {
     duration: "3 nights",
-    value: 0,
+    value: 2,
     color: "#eab308",
   },
   {
     duration: "4-5 nights",
-    value: 0,
+    value: 5,
     color: "#84cc16",
   },
   {
     duration: "6-7 nights",
-    value: 0,
+    value: 6,
     color: "#22c55e",
   },
   {
     duration: "8-14 nights",
-    value: 0,
+    value: 10,
     color: "#14b8a6",
   },
   {
     duration: "15-21 nights",
-    value: 0,
+    value: 8,
     color: "#3b82f6",
   },
   {
     duration: "21+ nights",
-    value: 0,
-    color: "#a855f7",
+    value: 4,
+    color: "#3965df",
   },
 ];
 
@@ -74,12 +84,12 @@ const startDataDark = [
   },
   {
     duration: "3 nights",
-    value: 0,
+    value: 3,
     color: "#a16207",
   },
   {
     duration: "4-5 nights",
-    value: 0,
+    value: 5,
     color: "#4d7c0f",
   },
   {
@@ -89,24 +99,22 @@ const startDataDark = [
   },
   {
     duration: "8-14 nights",
-    value: 0,
+    value: 8,
     color: "#0f766e",
   },
   {
     duration: "15-21 nights",
-    value: 0,
+    value: 21,
     color: "#1d4ed8",
   },
   {
     duration: "21+ nights",
-    value: 0,
-    color: "#7e22ce",
+    value: 21,
+    color: "#2281ce",
   },
 ];
 
 function prepareData(startData, stays) {
-  // A bit ugly code, but sometimes this is what it takes when working with real data 😅
-
   function incArrayValue(arr, field) {
     return arr.map((obj) =>
       obj.duration === field ? { ...obj, value: obj.value + 1 } : obj
@@ -130,3 +138,47 @@ function prepareData(startData, stays) {
 
   return data;
 }
+
+function DurationChart({ confirmedStays }) {
+  const { isDarkMode } = useDarkMode();
+  const startData = isDarkMode ? startDataDark : startDataLight;
+  const data = prepareData(startData, confirmedStays);
+  return (
+    <ChartBox>
+      <Heading as="h2">Stay duration summary</Heading>
+      <ResponsiveContainer>
+        <PieChart>
+          <Pie
+            data={data}
+            nameKey="duration"
+            dataKey="value"
+            innerRadius={85}
+            outerRadius={110}
+            cx="40%"
+            cy="50%"
+            paddingAngle={3}
+          >
+            {data.map((entry) => (
+              <Cell
+                fill={entry.color}
+                stroke={entry.color}
+                key={entry.duration}
+              />
+            ))}
+          </Pie>
+          <Tooltip />
+          <Legend
+            verticalAlign="middle"
+            align="right"
+            width="30%"
+            layout="vertical"
+            iconSize={15}
+            iconType="circle"
+          />
+        </PieChart>
+      </ResponsiveContainer>
+    </ChartBox>
+  );
+}
+
+export default DurationChart;
